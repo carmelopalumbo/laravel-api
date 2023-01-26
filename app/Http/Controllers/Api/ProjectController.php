83a4abee932b952cf50e848dbd4305ae67ee43b9
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-26 14:35:33 +0100

    add-api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with(['type', 'technologies'])->get();
+        return response()->json(compact('projects'));
+    }
+}
