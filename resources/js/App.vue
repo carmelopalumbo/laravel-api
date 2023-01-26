180e0277d74fe447de1a3fde0052c0962317d47a
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-26 17:22:29 +0100

    add-vue-into-project

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<template>
+    <div class="container pt-5">
+        <h1>TEST VUE INTO LARAVEL</h1>
+    </div>
+
+</template>
+
+<style>
+@import "../scss/appVue.scss";
+</style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.guest')
 @section('content')
-    <h1 class="text-center py-5">HOME GUEST SECTION</h1>
+    <div id="app"></div>
 @endsection
